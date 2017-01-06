d3b1bdf68a7b106c1a9d15f1e21007e706285c72
cleoqc <cleoqc1124@gmail.com>
2017-01-06 15:40:44 +0000

    fix the pull command

--- install_script_tools.sh
+++ install_script_tools.sh
@@ -20,7 +20,7 @@ fi
 # go into $DEXTER
 cd $DEXTER
 
-# check if lib exists, if not create it
+# check if /home/pi/Dexter/lib exists, if not create it
 if [ ! -d $LIB ] ; then
 	mkdir $LIB
 fi
@@ -28,7 +28,7 @@ fi
 # check if /home/pi/Dexter/lib/Dexter exists
 # if yes refresh the folder
 # if not, clone the folder
-if [ ! -d $DEXTER ] ; then
+if [ -d $DEXTER ] ; then
 	cd $DEXTER
 	sudo git pull
 else
