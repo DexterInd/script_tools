b1fb468eda44b8cf889e4494cc1fe3079dc1c7ab
cleoqc <cleoqc1124@gmail.com>
2017-01-06 15:38:56 +0000

    fix a couple of small mistakes

--- install_script_tools.sh
+++ install_script_tools.sh
@@ -14,21 +14,21 @@ LIB=lib
 pushd $PIHOME
 
 # check if ~/Dexter exists, if not create it
-if [ -d $DEXTER ] ; then
+if [ ! -d $DEXTER ] ; then
 	mkdir $DEXTER
 fi
 # go into $DEXTER
 cd $DEXTER
 
 # check if lib exists, if not create it
-if [ -d $LIB ] ; then
+if [ ! -d $LIB ] ; then
 	mkdir $LIB
 fi
 
 # check if /home/pi/Dexter/lib/Dexter exists
 # if yes refresh the folder
 # if not, clone the folder
-if [ -d $DEXTER] ; then
+if [ ! -d $DEXTER ] ; then
 	cd $DEXTER
 	sudo git pull
 else
