a18d44ded71fbed7bb6e0e2c8aeb0d3dde519730
cleoqc <cleoqc1124@gmail.com>
2017-01-06 15:36:03 +0000

    use this install script with curl to get the files installed

---
+++ install_script_tools.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+#####################################################################
+#####################################################################
+#
+# curl 
+#
+#####################################################################
+#####################################################################
+
+PIHOME=/home/pi
+DEXTER=Dexter
+LIB=lib
+
+pushd $PIHOME
+
+# check if ~/Dexter exists, if not create it
+if [ -d $DEXTER ] ; then
+	mkdir $DEXTER
+fi
+# go into $DEXTER
+cd $DEXTER
+
+# check if lib exists, if not create it
+if [ -d $LIB ] ; then
+	mkdir $LIB
+fi
+
+# check if /home/pi/Dexter/lib/Dexter exists
+# if yes refresh the folder
+# if not, clone the folder
+if [ -d $DEXTER] ; then
+	cd $DEXTER
+	sudo git pull
+else
+	# clone the folder
+	sudo git clone https://github.com/DexterInd/script_tools.git
+fi
+
+popd
