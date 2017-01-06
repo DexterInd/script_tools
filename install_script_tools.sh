27ebf2786a41d16873428c31b3693cfe73b27e0f
cleoqc <cleoqc1124@gmail.com>
2017-01-06 16:10:32 +0000

    is this one the right one?

--- install_script_tools.sh
+++ install_script_tools.sh
@@ -10,10 +10,10 @@
 PIHOME=/home/pi
 DEXTER=Dexter
 LIB=lib
-SCRIPTS=script_tools
+SCRIPT=script_tools
 
 pushd $PIHOME
-
+result=${PWD##*/} 
 # check if ~/Dexter exists, if not create it
 if [ ! -d $DEXTER ] ; then
 	echo "creating $PIHOME/$DEXTER"
@@ -21,6 +21,7 @@ if [ ! -d $DEXTER ] ; then
 fi
 # go into $DEXTER
 cd $DEXTER
+echo $PWD
 
 # check if /home/pi/Dexter/lib exists, if not create it
 if [ ! -d $LIB ] ; then
@@ -28,6 +29,15 @@ if [ ! -d $LIB ] ; then
 	mkdir $LIB
 fi
 cd $LIB
+echo $PWD
+
+# check if /home/pi/Dexter/lib/Dexter exists, if not create it
+if [ ! -d $DEXTER ] ; then
+	echo "creating $PIHOME/$DEXTER/$LIB/$Dexter"
+	mkdir $DEXTER
+fi
+cd $DEXTER
+echo $PWD
 
 # check if /home/pi/Dexter/lib/script_tools exists
 # if yes refresh the folder
@@ -35,6 +45,8 @@ cd $LIB
 if [ -d $SCRIPT ] ; then
 	echo "Pulling"
 	cd $SCRIPT
+	echo $PWD
+	echo "now in $PIHOME/$DEXTER/$LIB/$SCRIPT"
 	sudo git pull
 else
 	# clone the folder
