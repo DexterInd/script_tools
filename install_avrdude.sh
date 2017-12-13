d7d63635edbb47e082781c85fbc03e648805f211
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2017-12-13 20:42:22 +0200

    Revert "ref RobertLucian/script_tools#2 : hotfix - removed sudoes from all .sh"
    This reverts commit e347d91160b929e13aa06c86b3f6f2ed6c13b9d7.

--- install_avrdude.sh
+++ install_avrdude.sh
@@ -5,7 +5,7 @@ create_avrdude_folder(){
     AVRDUDE_DIR='/home/pi/Dexter/lib/AVRDUDE'
     if [ -d "$AVRDUDE_DIR" ]; then
         echo $AVRDUDE_DIR" Found!"
-    else
+    else 
         DIRECTORY='/home/pi/Dexter'
         if [ -d "$DIRECTORY" ]; then
             # Will enter here if $DIRECTORY exists, even if it contains spaces
@@ -23,7 +23,7 @@ create_avrdude_folder(){
             echo "creating "$DIRECTORY
             mkdir $DIRECTORY
         fi
-
+        
         pushd $DIRECTORY
         git clone https://github.com/DexterInd/AVRDUDE.git
         popd
@@ -35,36 +35,36 @@ install_avrdude(){
     FILENAME=tmpfile.txt
     AVRDUDE_VER=5.10
     avrdude -v &> $FILENAME
-
+    
     #Only install avrdude 5.1 if it does not exist
-    if grep -q $AVRDUDE_VER $FILENAME
+    if grep -q $AVRDUDE_VER $FILENAME 
     then
         echo "avrdude" $AVRDUDE_VER "Found"
     else
         echo "avrdude" $AVRDUDE_VER "Not Found,Installing avrdude now"
         create_avrdude_folder
-
+        
         ##########################################
         #Installing AVRDUDE
         ##########################################
         pushd /home/pi/Dexter/lib/AVRDUDE/avrdude
-
+        
         # Install the avrdude deb package
         # No need to wget since files should be there in the avrdude folder
         # wget https://github.com/DexterInd/AVRDUDE/raw/master/avrdude/avrdude_5.10-4_armhf.deb
-        dpkg -i avrdude_5.10-4_armhf.deb
-        chmod 4755 /usr/bin/avrdude
-
-        # Setup config files
+        sudo dpkg -i avrdude_5.10-4_armhf.deb 
+        sudo chmod 4755 /usr/bin/avrdude
+        
+        # Setup config files 
         # wget http://project-downloads.drogon.net/gertboard/setup.sh
         chmod +x setup.sh
-        ./setup.sh  
-
+        sudo ./setup.sh  
+        
         # pushd /etc/minicom
         # sudo wget http://project-downloads.drogon.net/gertboard/minirc.ama0
         # sudo sed -i '/Exec=arduino/c\Exec=sudo arduino' /usr/share/applications/arduino.desktop
         echo " "
         popd
     fi
-    rm $FILENAME
+    rm $FILENAME   
 }
