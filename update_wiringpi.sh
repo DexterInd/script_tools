9a542971a5cacbe65da8610de1e439718c0b7e55
Nicole Parrot <cleo@parrot.ca>
2022-07-07 15:58:21 -0400

    Get new wiringpi

--- update_wiringpi.sh
+++ update_wiringpi.sh
@@ -6,45 +6,48 @@ DEXTER=Dexter
 LIB=lib
 
 
-# Check if WiringPi Installed and has the latest version.  If it does, skip the step.
-# Gets the version of wiringPi installed
-version=`gpio -v`       
+# # Check if WiringPi Installed and has the latest version.  If it does, skip the step.
+# # Gets the version of wiringPi installed
+# version=`gpio -v`
 
-# Parses the version to get the number
-set -- $version         
+# # Parses the version to get the number
+# set -- $version
 
-# Gets the third word parsed out of the first line of gpio -v returned.
-# Should be 2.36
-WIRINGVERSIONDEC=$3     
+# # Gets the third word parsed out of the first line of gpio -v returned.
+# # Should be 2.52
+# WIRINGVERSIONDEC=$3
 
-# Store to temp file
-echo $WIRINGVERSIONDEC >> tmpversion    
+# # Store to temp file
+# echo $WIRINGVERSIONDEC >> tmpversion
 
-# Remove decimals
-VERSION=$(sed 's/\.//g' tmpversion)     
+# # Remove decimals
+# VERSION=$(sed 's/\.//g' tmpversion)
 
-# Remove the temp file
-delete_file tmpversion                           
+# # Remove the temp file
+# delete_file tmpversion
 
-feedback "wiringPi VERSION is $VERSION"
-if [ $VERSION -eq '236' ]; then
+# feedback "wiringPi VERSION is $VERSION"
+# if [ $VERSION -eq '252' ]; then
 
-    feedback "FOUND WiringPi Version 2.36 No installation needed."
-else
-    feedback "Did NOT find WiringPi Version 2.36"
-    # Check if the Dexter directory exists.
-    create_folder "$PIHOME/$DEXTER"
-    create_folder "$PIHOME/$DEXTER/$LIB"
+#     feedback "FOUND WiringPi Version 2.52 No installation needed."
+# else
+#     feedback "Did NOT find WiringPi Version 2.52"
+#     # Check if the Dexter directory exists.
+#     create_folder "$PIHOME/$DEXTER"
+#     create_folder "$PIHOME/$DEXTER/$LIB"
 
-    # Change directories to Dexter/lib
-    cd $PIHOME/$DEXTER/$LIB
-    
-    delete_folder wiringPi  
-    # Install wiringPi
-    git clone https://github.com/DexterInd/wiringPi/  # Clone directories to Dexter.
-    cd wiringPi
-    sudo chmod +x ./build
-    sudo ./build
-    feedback "wiringPi Installed"
-fi
-# End check if WiringPi installed
+#     # Change directories to Dexter/lib
+#     cd $PIHOME/$DEXTER/$LIB
+
+#     delete_folder wiringPi
+#     # Install wiringPi
+#     git clone https://github.com/DexterInd/wiringPi/  # Clone directories to Dexter.
+#     cd wiringPi
+#     sudo chmod +x ./build
+#     sudo ./build
+#     feedback "wiringPi Installed"
+# fi
+# # End check if WiringPi installed
+
+
+sudo python -m pip install wiringPi==2.60.0
